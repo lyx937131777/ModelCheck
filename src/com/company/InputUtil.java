8c4cb058af1def91386dc06e28a0cebba99169cd
savior2 <45728546+savior2@users.noreply.github.com>
2021-01-02 10:18:33 +0800

    增加输入

--- src/com/company/InputUtil.java
+++ src/com/company/InputUtil.java
@@ -1,7 +1,36 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 //输入
 public class InputUtil {
     //TODO 文件读入 文件中原子先用abcd表示 CTL中算子也用0123表示
     //TODO 我倾向于model和CTL在同一个文件中输入 前面是model 后面再一个数字m表示CTL的个数 后面m行CTL
+    public static String readFile(String fileName) {
+        File f = new File(fileName);
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(f));
+            String s = null;
+            StringBuilder sb = new StringBuilder();
+            while ((s = reader.readLine()) != null) {
+                sb.append(s).append("\n");
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,42 +8,47 @@ import java.util.Map;
 //系统分析与验证作业 主入口
 public class Main {
 
-    private static Map<String,Boolean> m = new HashMap<>();
+    private static Map<String, Boolean> m = new HashMap<>();
 
     public static void main(String[] args) {
-	    OutputUtil.out("model check start!");
-        Model model = new Model(5);
-        model.setP(0,"a");
-        model.setP(0,"b");
-        model.setP(1,"a");
-        model.setP(2,"b");
-        model.setP(3,"a");
-        model.setP(4,"b");
-        model.addEdge(0,1);
-        model.addEdge(1,2);
-        model.addEdge(0,2);
-        model.addEdge(2,1);
-        model.addEdge(2,2);
-        model.addEdge(3,4);
-        model.addEdge(4,3);
-        List<CTL> ctlList = new ArrayList<>();
-        ctlList.add(new CTL("a"));
-        ctlList.add(new CTL("b"));
-        ctlList.add(new CTL("a0"));
-        ctlList.add(new CTL("ab1"));
-        ctlList.add(new CTL("a0b010"));
-        ctlList.add(new CTL("a2"));
-        ctlList.add(new CTL("a3"));
-        ctlList.add(new CTL("ba4"));
-        ctlList.add(new CTL("ba1a0b14"));
-        ctlList.add(new CTL("ab5"));
-        ctlList.add(new CTL("ba1a0b15"));
-        for(CTL ctl : ctlList){
-            System.out.println("CTL: " + ctl.toString());
-            for(int s = 0; s < model.getCount(); s++){
-                System.out.println("state: " + s + " result: " + model.verify(s,ctl));
-            }
-        }
-        model.printM();
+        String text = InputUtil.readFile("data/data.txt");
+        assert text != null;
+        String[] textArray = text.split("\n");
+
+
+//	    OutputUtil.out("model check start!");
+//        Model model = new Model(5);
+//        model.setP(0,"a");
+//        model.setP(0,"b");
+//        model.setP(1,"a");
+//        model.setP(2,"b");
+//        model.setP(3,"a");
+//        model.setP(4,"b");
+//        model.addEdge(0,1);
+//        model.addEdge(1,2);
+//        model.addEdge(0,2);
+//        model.addEdge(2,1);
+//        model.addEdge(2,2);
+//        model.addEdge(3,4);
+//        model.addEdge(4,3);
+//        List<CTL> ctlList = new ArrayList<>();
+//        ctlList.add(new CTL("a"));
+//        ctlList.add(new CTL("b"));
+//        ctlList.add(new CTL("a0"));
+//        ctlList.add(new CTL("ab1"));
+//        ctlList.add(new CTL("a0b010"));
+//        ctlList.add(new CTL("a2"));
+//        ctlList.add(new CTL("a3"));
+//        ctlList.add(new CTL("ba4"));
+//        ctlList.add(new CTL("ba1a0b14"));
+//        ctlList.add(new CTL("ab5"));
+//        ctlList.add(new CTL("ba1a0b15"));
+//        for(CTL ctl : ctlList){
+//            System.out.println("CTL: " + ctl.toString());
+//            for(int s = 0; s < model.getCount(); s++){
+//                System.out.println("state: " + s + " result: " + model.verify(s,ctl));
+//            }
+//        }
+//        model.printM();
     }
 }
